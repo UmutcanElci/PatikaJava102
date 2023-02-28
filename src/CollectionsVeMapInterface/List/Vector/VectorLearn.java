f6e92e13d820371292f74a50ecc4b9b4129285d8
Umutcan Elçi <umutcanelci@yaani.com>
2023-02-28 15:26:30 +0300

    Vector

---
+++ src/CollectionsVeMapInterface/List/Vector/VectorLearn.java
@@ -0,0 +1,25 @@
+package CollectionsVeMapInterface.List.Vector;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+public class VectorLearn {
+    public static void main(String[] args) {
+        Vector<String> vector = new Vector<>();
+        vector.add("Umut");
+        vector.add("Ozan");
+        vector.add("124");
+        vector.add("A");
+
+        vector.add(2,"Bolu");
+        vector.remove(2);
+        System.out.println(vector.get(3));
+        System.out.println(vector.capacity());
+
+        Iterator<String> iterator = vector.iterator();
+
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+}
