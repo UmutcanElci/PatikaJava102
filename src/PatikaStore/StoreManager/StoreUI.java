5590a78834a91de75afea0876319f37fa2618af9
Umutcan Elçi <umutcanelci@yaani.com>
2023-03-04 02:20:02 +0300

    Store-UI

--- src/PatikaStore/StoreManager/StoreUI.java
+++ src/PatikaStore/StoreManager/StoreUI.java
@@ -4,8 +4,6 @@ import PatikaStore.Brand.Brands;
 import PatikaStore.Product.NoteBooks;
 import PatikaStore.Product.Products;
 
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class StoreUI implements IStoreManager {
@@ -13,14 +11,29 @@ public class StoreUI implements IStoreManager {
 
     @Override
     public void printBrands() {
-        NoteBooks n = new NoteBooks();
-        n.add();
+        brands.printBrands();
     }
 
     @Override
     public void ListNoteBooks() {
-
+        noteBooks.listNoteBooks();
     }
+
+    @Override
+    public void addNoteBooks() {
+        noteBooks.addNoteBook();
+    }
+
+    @Override
+    public void deleteNoteBookById() {
+        noteBooks.deleteById();
+    }
+
+    @Override
+    public void findNoteBookById() {
+        noteBooks.listById();
+    }
+
 
     @Override
     public void ListPhones() {
