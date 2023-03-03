57dbc2b7bf69d62f91af512222c180a74189ac7b
Umutcan Elçi <umutcanelci@yaani.com>
2023-03-04 02:20:52 +0300

    PatikStore

--- src/PatikaStore/PatikaStore.java
+++ src/PatikaStore/PatikaStore.java
@@ -1,12 +1,11 @@
 package PatikaStore;
 
-import PatikaStore.Product.Prodcuts;
 import PatikaStore.StoreManager.StoreUI;
 
 public class PatikaStore {
     public static void main(String[] args) {
         StoreUI storeUI = new StoreUI();
-        storeUI.printBrands();
-
+        storeUI.addNoteBooks();
+        storeUI.ListNoteBooks();
     }
 }
