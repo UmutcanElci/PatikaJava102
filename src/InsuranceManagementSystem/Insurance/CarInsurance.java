b65a1c19bfdd017521486ed3fa8ac114297b4273
Umutcan Elçi <umutcanelci@yaani.com>
2023-03-07 21:30:47 +0300

    Car Insurance

--- src/InsuranceManagementSystem/Insurance/CarInsurance.java
+++ src/InsuranceManagementSystem/Insurance/CarInsurance.java
@@ -2,7 +2,9 @@ package InsuranceManagementSystem.Insurance;
 
 public class CarInsurance extends Insurance{
     @Override
-    public void calculate() {
-        
+    public double calculate(double insurancePrice) {
+        return insurancePrice*35;
     }
+
+
 }
