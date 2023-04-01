ca11796768105d508980682f9403111a700ef912
Umutcan Elçi <umutcanelci@yaani.com>
2023-04-01 20:27:40 +0300

    Volatile

---
+++ src/Threads/VolatileKeyWord/VolatileLearn.java
@@ -0,0 +1,10 @@
+package Threads.VolatileKeyWord;
+
+public class VolatileLearn {
+    /*
+    “volatile” anahtar kelimesi değişkenin sakladığı değerin Thread’ler tarafından okunmaya çalışıldığında hepsinde aynı değerin okunacağının garantisini verir.
+    Bilgisayar mimarisinde ana hafıza bölgesi vardır. Bir program çalıştığı süre boyunca işlediği veriler ve program komutları bu ana hafıza bölgesinde saklanır.
+
+
+     */
+}
